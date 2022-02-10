a4eff6585743bff1cb35847efb1e13eacf628ace
Wayt <01flax01@gmail.com>
2022-02-10 21:24:57 +0200

    Update tasks

--- assets/js/context.js
+++ assets/js/context.js
@@ -1,3 +1,8 @@
+/* 1) создать обьект cat1
+в свойство name записать имя
+также добавить метод sayHi который приветствует по имени
+ */
+
 const cat1 = {
   name: 'Vasya',
   sayHi: function (name) {
@@ -6,7 +11,11 @@ const cat1 = {
 }
 cat1.sayHi()
 
-//////////////////////////////////////////////////
+
+
+/* 2) создать второй обьект cat2 с той же конструкцией и другим именем
+у cat2 значение свойства sayHi на заменить функцию
+которая логирует cat1.name и this.name */
 
 const cat2 = {
   name: 'Oleg',
@@ -16,11 +25,62 @@ const cat2 = {
 }
 cat2.sayHi();
 
-///////////////////////////////////////////////////
+
+/* 3) создать функцию которая принимает обьект cat в параметры и меняет свойство name на произвольное */
 
 function funcCat(cat) {
   cat1.name = cat;
+  return cat1;
 }
 
 const resultCat = funcCat('Anna');
-console.log(cat1);
+console.log(cat1.name);
+
+
+
+/* 4) создать стрелочную функцию
+которая логирует this.name
+и записать ее ссылку в свойство cat2.sayHi
+вызвать cat2.sayHi - почему имя не логируется? */
+
+const user = {
+  name: 'Asher',
+  sayHi() {
+    const arrName = () =>(this.name);
+    cat2.sayHi = user.name;
+  }
+};
+
+user.sayHi();
+
+console.log(cat2.sayHi);
+
+
+
+/* 5) функция конструктор Article которая имеет свойства названия, и массив авторов, а также метод для добавления новых авторов
+this.name
+this.authors
+this.addAuthor = function*/
+
+function Article(name, authors) {
+  this.name = name;
+  this.authors = authors;
+  this.addAuthor = function() {};
+}
+
+const testConst = new Article('Vanya', 'Автор');
+
+console.log(testConst);
+testConst.addAuthor = 'AddTestAuthor'; 
+
+
+
+
+
+/* 6) вызвать функцию
+cat1.sayHi.call(cat2)
+почему имя в консоли не взято от cat1 а принадлежит cat2 */
+
+cat1.sayHi.call(cat2);
+
+// MDN - Function.prototype.call() 
