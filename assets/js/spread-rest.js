8228e4c7945900c3a69b7cbf23b04ea25ecc430e
Wayt <01flax01@gmail.com>
2022-02-21 11:43:30 +0200

    Update home work

--- assets/js/spread-rest.js
+++ assets/js/spread-rest.js
@@ -3,9 +3,6 @@
 // [ 1, 2, 3, ...pasteArray, 5, 6 ]
 // { field1Name: 'value1', field2Name: 'value2', ...mergeWithObj }
 
-
-
-
 // Массивы
 //1) создать массив с произвольными данными (* случайными числами),
 
@@ -13,55 +10,41 @@ const arrRandom = [12, 2, 23, 4, 6];
 
 console.log(arrRandom);
 
-
-
-
 //2) с помощью оператора spread клонируйте массив в новый
 
 arrCopy = [...arrRandom];
 console.log(arrCopy);
 
-
-
-
 /*3)используя оператор spread создайте новый массив из букв строки "тест строки как массива"
 (разбейте строку как массив)*/
 
-const str = [...'тест строки как массива'];
+const str = [..."тест строки как массива"];
 
 console.log(str);
-
-
 
 //Обьекты
 /* 4) создайте объект со свойствами:
 id, firstname, lastname, age, */
 
 const userObj = {
-  id: '23',
-  firstname: 'Vasya',
-  age: '23',
-}
-
-
+  id: "23",
+  firstname: "Vasya",
+  age: "23",
+};
 
 /* 5) создайте второй объект со свойствами
 tel, email */
 
 const userObjPhone = {
-  tel: '09333333',
-  email: 'email@gmail.com',
-}
-
+  tel: "09333333",
+  email: "email@gmail.com",
+};
 
 //6) с помощью оператора spread объедините оба объекта в новый и сохраните в переменную
- 
-const userResultObj = {...userObj,...userObjPhone}
+
+const userResultObj = { ...userObj, ...userObjPhone };
 
 console.log(userResultObj);
-
-
-
 
 //Оператор Rest:
 // function (...myRestName) {}
@@ -76,28 +59,33 @@ function restArg(...unlimitedArg) {
   console.log(unlimitedArg);
 }
 
-restArg('test');
-
+restArg("test");
 
 /* 2) создайте функцию чтобы она принимала любое кол-во параметров:
 если в какой либо ячейке есть число то квадрат этого числа
 иначе если в какой либо ячейке есть строка то возвращала эту строку не изменяя */
-/* 
-function paramPow (firstParam, ...param) {
-  if (typeof firstParam === 'number' || typeof param === 'number') {
-    const resultPow = Math.pow(firstParam,param)                              //Not working
-    return resultPow;
-  } if (typeof firstParam === 'string' || typeof param === 'string') {
-    return eval();
-  }
-}
 
-const resultPar = paramPow('a',4);
-console.log(resultPar);
- */
+/* function paramPow(...param) {
+  const arr = [];
+
+  param.forEach(function (value) {
+    if (typeof value === 'number') {
+      const resultPow = Math.pow(value, 2); 
+      return arr.push(resultPow);
+    }
+    if (typeof value === 'string') {
+      return arr.push(value);
+    }
+  });
+  return arr;
+} */
+
+/* const resultPar = paramPow(4, 'asd');
+console.log(resultPar); */
 
 
-// 3) создайте функцию которая находит минимальное число из всех полученных аргументов 
+
+// 3) создайте функцию которая находит минимальное число из всех полученных аргументов
 
 /* function minNum (...arg) {
   const sumMin = Math.min(...arg);
